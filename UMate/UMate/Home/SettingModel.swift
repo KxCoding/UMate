4ff26247c81067344440d5b47d3b1cba01d4db7d
TaekToy <sinadsl1457@gmail.com>
2021-07-23 01:58:00 +0900

    [Add]설정 ui추가

---
+++ UMate/UMate/Home/SettingModel.swift
@@ -0,0 +1,13 @@
+//
+//  SettingModel.swift
+//  SettingModel
+//
+//  Created by 황신택 on 2021/07/23.
+//
+
+import Foundation
+
+enum CellType: String {
+    case profile
+    
+}
---
+++ UMate/UMate/Home/SettingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SettingViewController.swift
+//  SettingViewController
+//
+//  Created by 황신택 on 2021/07/23.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+   
+    }
+    
+
+   
+}
