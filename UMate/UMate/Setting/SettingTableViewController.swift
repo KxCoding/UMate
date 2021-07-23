967c73c171ac6cf07cd4cf3d437d8e17b0c92a87
TaekToy <sinadsl1457@gmail.com>
2021-07-24 00:27:58 +0900

    testcommit

---
+++ UMate/UMate/Setting/SettingTableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SettingTableViewController.swift
+//  SettingTableViewController
+//
+//  Created by 황신택 on 2021/07/23.
+//
+
+import UIKit
+
+class SettingTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+
+}
