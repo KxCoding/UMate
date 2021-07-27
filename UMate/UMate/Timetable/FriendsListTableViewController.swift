a5ffd2dee4aed7aa2358cddb852cf506f1c274bd
sanghee <sanghee0610@ajou.ac.kr>
2021-07-28 03:22:45 +0900

    [Add] 친구 목록 / 친구 시간표 화면 추가

---
+++ UMate/UMate/Timetable/FriendsListTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FriendsListTableViewController.swift
+//  FriendsListTableViewController
+//
+//  Created by 안상희 on 2021/07/28.
+//
+
+import UIKit
+
+class FriendsListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
