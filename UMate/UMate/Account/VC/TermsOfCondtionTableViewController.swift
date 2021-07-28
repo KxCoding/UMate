be5605fe93ea191767ba90aa5c74a29305cdbbe0
TaekToy <sinadsl1457@gmail.com>
2021-07-29 03:11:20 +0900

    [Add]이용약관 UI 구현 + 체크박스 세부적인 기능 구현

---
+++ UMate/UMate/Account/VC/TermsOfCondtionTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TermsOfCondtionTableViewController.swift
+//  UMate
+//
+//  Created by 황신택 on 2021/07/28.
+//
+
+import UIKit
+
+class TermsOfCondtionTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+   
+   
+}
