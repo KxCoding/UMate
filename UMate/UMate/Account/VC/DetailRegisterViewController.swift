eaa79afe6bb31104b050e71a40be98553e2ea242
TaekToy <sinadsl1457@gmail.com>
2021-08-06 19:18:43 +0900

    [Add]프로필 사진 업로드시 디바이스에 따라서 사진 오류 수정/ 학교 리스트추가/ 텍스트필드 키보드 커버  보이게 수정

--- UMate/UMate/Account/VC/DetailRegisterViewController.swift
+++ UMate/UMate/Account/VC/DetailRegisterViewController.swift
@@ -15,8 +15,9 @@ class DetailRegisterViewController: UIViewController {
     @IBOutlet weak var nickNameTextField: UITextField!
     @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var profileImageView: UIImageView!
-    
     @IBOutlet weak var changeProfilePicButton: UIButton!
+
+    var activeTextField: UITextField? = nil
     var verifiedEmail: String?
     
     
@@ -25,31 +26,39 @@ class DetailRegisterViewController: UIViewController {
               isPasswordValid(password),
               password.count >= 8,
               password.trimmingCharacters(in: .whitespacesAndNewlines) != "" else {
-                  alertWithNoAction(title: "알림", message: "비밀번호는 반드시 영문 숫자 특수문자를 포함해야합니다냐.")
-                  return
-              }
+            alertWithNoAction(title: "알림", message: "비밀번호는 반드시 영문 숫자 특수문자를 포함해야합니다냐.")
+            return
+        }
         guard let repeatPassword = repeatPasswordTextField.text,
               repeatPassword == password else {
-                  alertWithNoAction(title: "알림", message: "비밀번호가 같지 않습니다.")
-                  return
-              }
+            alertWithNoAction(title: "알림", message: "비밀번호가 같지 않습니다.")
+            return
+        }
         guard let name = nickNameTextField.text,
               let nickName = nickNameTextField.text,
               name.count >= 2, nickName.count >= 2 else {
-                  alertWithNoAction(title: "알림", message: "잘못된 형식의 이름 혹은 닉네임입니다.")
-                  return
-              }
+            alertWithNoAction(title: "알림", message: "잘못된 형식의 이름 혹은 닉네임입니다.")
+            return
+        }
         
         
-        
+        transitionToHome()
         
     }
     
+    func transitionToHome() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let mainTabBarController = storyboard.instantiateViewController(withIdentifier: "MainTabBarController")
+        
+        (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(mainTabBarController)
+    }
     
     var token: NSObjectProtocol?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        keyboardWillShow()
+        keyboardWillHide()
         [emailTextField, passwordTextField, repeatPasswordTextField, nameTextField, nickNameTextField, nextButton, changeProfilePicButton].forEach({
             $0?.layer.cornerRadius = 10
             $0?.clipsToBounds = true
@@ -59,6 +68,7 @@ class DetailRegisterViewController: UIViewController {
             emailTextField.text = verifiedEmail
         }
         
+ 
         token = NotificationCenter.default.addObserver(forName: .didTapProfilePics, object: nil, queue: .main, using: { [weak self] noti  in
             guard let strongSelf = self else { return }
             guard let profileImageView = noti.userInfo?["picsKey"] as? UIImageView else { return }
@@ -70,6 +80,17 @@ class DetailRegisterViewController: UIViewController {
         profileImageView.layer.cornerRadius = profileImageView.frame.width / 2.0
         profileImageView.clipsToBounds = true
         profileImageView.contentMode = .scaleAspectFill
+        
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(DetailRegisterViewController.backgroundTap))
+        self.view.addGestureRecognizer(tapGestureRecognizer)
+        nameTextField.delegate = self
+        repeatPasswordTextField.delegate = self
+        nickNameTextField.delegate = self
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        
+        
         
         
     }
@@ -87,3 +108,60 @@ class DetailRegisterViewController: UIViewController {
     }
     
 }
+
+extension DetailRegisterViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        activeTextField = textField
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        activeTextField = nil
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if emailTextField != nil {
+            return false
+        }
+        return true
+    }
+}
+
+
+extension DetailRegisterViewController {
+    
+    func keyboardWillShow() {
+        NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: .main) { [weak self] noti in
+            guard let strongSelf = self else { return  }
+            guard let keyboardSize = (noti.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else { return }
+        
+            var shouldMoveViewUp = false
+            
+            if let activeTextField = strongSelf.activeTextField {
+                let bottomOfTextField = activeTextField.convert(activeTextField.bounds, to: strongSelf.view).maxY
+                let topOfKeyboard = strongSelf.view.frame.height - keyboardSize.height
+                
+                if bottomOfTextField > topOfKeyboard {
+                    shouldMoveViewUp = true
+                }
+            }
+            
+            if shouldMoveViewUp {
+                strongSelf.view.frame.origin.y = 0 - keyboardSize.height
+            }
+        }
+    }
+    
+    func keyboardWillHide() {
+        NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: .main) { [weak self] noti in
+            guard let strongSelf = self else { return }
+            strongSelf.view.frame.origin.y = 0
+        }
+    }
+    
+    
+    @objc func backgroundTap(_ sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
+}
+
+
