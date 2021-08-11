aec75e21e67c65a8919d2bbd748b1dcf9b0db38b
Hyunwoo <heoun3089@gmail.com>
2021-08-12 04:29:03 +0900

    [Place-Add/Design] Asset 파일 추가 + Search 데이터 모델 수정
    - Filter화면 가게 종류 이미지 Template 이미지로 변경
    - SearchPlaceItem에 PlaceType 열거형 추가

--- UMate/UMate/Place/Model/Search.swift
+++ UMate/UMate/Place/Model/Search.swift
@@ -13,4 +13,14 @@ struct SearchPlaceItem {
     let placeTitle: String
     let regionName: String
     let classificationName: String
+    
+    enum PlaceType: String {
+        case cafe = "cafe"
+        case restaurant = "restaurant"
+        case bakery = "bakery"
+        case studyCafe = "pencil"
+        case pub = "beer"
+        case desert = "desert"
+    }
+    let placeType: PlaceType
 }
