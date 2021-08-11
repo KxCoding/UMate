322e3921a55e5178be6e4bedfc0fac265da8af30
sanghee <sanghee0610@ajou.ac.kr>
2021-08-11 22:44:10 +0900

    [Extension-Add/Timetable-Refactor] Date Extension에 속성 추가 및 시간표 추가 코드 리팩토링

--- UMate/UMate/Timetable/ViewController/AddLectureTableViewController.swift
+++ UMate/UMate/Timetable/ViewController/AddLectureTableViewController.swift
@@ -25,23 +25,30 @@ class AddLectureTableViewController: UITableViewController {
     let buttonImage = UIImage(named: "check")
     
     
+    // Outlet - 강의 ID, 강의명, 강의실, 교수님 이름을 입력하기 위한 TextField
     @IBOutlet weak var courseIdField: UITextField!
     @IBOutlet weak var courseNameField: UITextField!
     @IBOutlet weak var roomNameField: UITextField!
     @IBOutlet weak var professorField: UITextField!
     
+    
+    // Outlet - 요일을 나타내는 UIView
     @IBOutlet weak var mondayContainerView: UIView!
     @IBOutlet weak var tuesdayContainerView: UIView!
     @IBOutlet weak var wednesdayContainerView: UIView!
     @IBOutlet weak var thursdayContainerView: UIView!
     @IBOutlet weak var fridayContainerView: UIView!
     
+    
+    // Outlet - 요일을 나타내는 UILabel
     @IBOutlet weak var mondayLabel: UILabel!
     @IBOutlet weak var tuesdayLabel: UILabel!
     @IBOutlet weak var wednesdayLabel: UILabel!
     @IBOutlet weak var thursdayLabel: UILabel!
     @IBOutlet weak var fridayLabel: UILabel!
     
+    
+    // Outlet - 색상을 선택하기 위한 UIView
     @IBOutlet weak var lightRedView: UIView!
     @IBOutlet weak var redView: UIView!
     @IBOutlet weak var pinkView: UIView!
@@ -55,6 +62,8 @@ class AddLectureTableViewController: UITableViewController {
     @IBOutlet weak var purpleView: UIView!
     @IBOutlet weak var darkGrayView: UIView!
     
+    
+    // Outlet - 색상을 선택하기 위한 UIButton
     @IBOutlet weak var lightRedButton: UIButton!
     @IBOutlet weak var redButton: UIButton!
     @IBOutlet weak var pinkButton: UIButton!
@@ -185,88 +194,39 @@ class AddLectureTableViewController: UITableViewController {
         
         initializeColorButton()
         
-        switch sender.tag {
-        case 200:
-            selectedButton(sender: sender)
-            
-            textColorString = "white"
-            
-        case 201:
-            selectedButton(sender: sender)
-            
-            textColorString = "white"
-            
-        case 202:
-            selectedButton(sender: sender)
-            
-            textColorString = "black"
-            
-        case 203:
-            selectedButton(sender: sender)
-            
-            textColorString = "black"
-            
-        case 204:
-            selectedButton(sender: sender)
-            
-            textColorString = "black"
-            
-        case 205:
-            selectedButton(sender: sender)
-            
-            textColorString = "black"
-            
-        case 206:
-            selectedButton(sender: sender)
-            
-            textColorString = "white"
-            
-        case 207:
-            selectedButton(sender: sender)
-            
-            textColorString = "black"
-            
-        case 208:
-            selectedButton(sender: sender)
-            
-            textColorString = "white"
-
-        case 209:
-            selectedButton(sender: sender)
-            
-            textColorString = "black"
-
-        case 210:
-            selectedButton(sender: sender)
-            
-            textColorString = "white"
-            
-        case 211:
-            selectedButton(sender: sender)
-            
-            textColorString = "white"
-            
-        default:
-            break
-        }
+        selectedButton(sender: sender)
         
+        
+        switch sender.tag {
+        /// case 200, 201, 206, 208, 210, 211의 경우는 textColor을 흰색으로 설정합니다.
+        case 200, 201, 206, 208, 210, 211:
+            textColorString = "white"
+            
+        /// 그 외의 경우는 모두 textColor을 검은색으로 설정합니다.
+        default:
+            textColorString = "black"
+        }
     }
     
     
+    /// 시간표 추가 화면에서 강의 시작 시간이 선택되면 호출되는 메소드입니다.
+    /// - Parameter sender: UIDatePicker
     @IBAction func startTimeDidSelected(_ sender: UIDatePicker) {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "HH:mm"
-        startTime = dateFormatter.string(from: sender.date)
+        startTime = sender.date.timeTableTime
     }
     
     
+    /// 시간표 추가 화면에서 강의 종료 시간이 선택되면 호출되는 메소드입니다.
+    /// - Parameter sender: UIDatePicker
     @IBAction func endTimeDidSelected(_ sender: UIDatePicker) {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "HH:mm"
-        endTime = dateFormatter.string(from: sender.date)
+        endTime = sender.date.timeTableTime
     }
     
     
+    /// textField의 길이를 체크하는 메소드입니다.
+    /// - Parameters:
+    ///   - textField: UITextField
+    ///   - maxLength: 최대 글자 수
     func checkMaxLength(textField: UITextField!, maxLength: Int) {
         if (textField.text?.count ?? 0 > maxLength) {
             textField.deleteBackward()
@@ -310,6 +270,8 @@ class AddLectureTableViewController: UITableViewController {
     }
     
     
+    /// 시간표에 데이터를 저장할 때 호출되는 메소드입니다.
+    /// - Parameter sender: UIBarButtonItem
     @IBAction func save(_ sender: Any) {
         // 시간표에 데이터 전달하기 위한 값
         guard let courseId = courseIdField.text, courseId.count > 0,
@@ -429,6 +391,15 @@ class AddLectureTableViewController: UITableViewController {
 
 extension AddLectureTableViewController: UITextFieldDelegate {
     
+    /// 지정된 텍스트를 변경할 것인지 delegate에게 묻는 메소드입니다.
+    ///
+    /// 강의명의 글자 수 입력을 20자로 제한합니다.
+    ///
+    /// - Parameters:
+    ///   - textField: 텍스트를 포함하고 있는 TextField.
+    ///   - range: 지정된 문자 범위입니다.
+    ///   - string: 지정된 범위에 대한 대체 문자열입니다. 
+    /// - Returns: Bool
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         guard let text = textField.text else { return true }
         
@@ -438,6 +409,9 @@ extension AddLectureTableViewController: UITextFieldDelegate {
     }
     
     
+    /// Return 버튼을 눌렀을 때의 process에 대해 delegate에게 묻는 메소드입니다.
+    /// - Parameter textField: Return 버튼이 눌려진 TextField.
+    /// - Returns: TextField가 return 버튼에 대한 동작을 구현해야하는 경우 true이고, 그렇지 않으면 false입니다.
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         if textField == courseIdField {
             courseNameField.becomeFirstResponder()
