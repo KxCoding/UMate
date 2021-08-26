79f7197a0e7c071be92d2ebc366dd441754437e0
TaekToy <sinadsl1457@gmail.com>
2021-08-26 16:09:39 +0900

    Revert "[Account-Add, SecureStore-Add] 키체인 구현, SecureStore구현, 코드 리팩토링 , 주석 추가"
    This reverts commit 7c5e8c0787740e266b797f5a4940842d40dc7459.

--- UMate/UMate/Account/VC/DetailRegisterViewController.swift
+++ UMate/UMate/Account/VC/DetailRegisterViewController.swift
@@ -6,16 +6,8 @@
 //
 
 import UIKit
-import KeychainSwift
-
-struct Keys {
-    static let prefixKey = "prefixKey"
-    static let passwordKey = "passwordKey"
-    static let userEmailKey = "userEmailKey"
-}
 
 class DetailRegisterViewController: UIViewController {
-    
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var repeatPasswordTextField: UITextField!
@@ -25,17 +17,10 @@ class DetailRegisterViewController: UIViewController {
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var changeProfilePicButton: UIButton!
 
-    /// To configure specific textfield
     var activeTextField: UITextField? = nil
-    
-    /// To save email data
     var verifiedEmail: String?
     
-    /// declarion Keychain Instance
-    var keyChain = KeychainSwift(keyPrefix: Keys.prefixKey)
     
-    /// Register Button
-    /// - Parameter sender: nextButton
     @IBAction func nextButtonAction(_ sender: Any) {
         guard let password = passwordTextField.text,
               isPasswordValid(password),
@@ -43,20 +28,11 @@ class DetailRegisterViewController: UIViewController {
                   alert(title: "알림", message: "비밀번호는 반드시 영문 숫자 특수문자를 포함해야합니다냐.")
             return
         }
-        
-        if keyChain.set(password, forKey: Keys.passwordKey, withAccess: .accessibleAfterFirstUnlock) {
-            print("Set")
-        } else {
-            print("Faild Set")
-        }
-        
-        
         guard let repeatPassword = repeatPasswordTextField.text,
               repeatPassword == password else {
                   alert(title: "알림", message: "비밀번호가 같지 않습니다.")
             return
         }
-    
         guard let name = nameTextField.text,
               let nickName = nickNameTextField.text,
               name.count >= 2, nickName.count >= 2 else {
@@ -64,41 +40,33 @@ class DetailRegisterViewController: UIViewController {
             return
         }
         
-        // Save name, nickName data in userDefaults
         UserDefaults.standard.set(name, forKey: "nameKey")
         UserDefaults.standard.set(nickName, forKey: "nickNameKey")
         
         
-        // Make transition to HomeViewController
         CommonViewController.shared.transitionToHome()
         
     }
     
 
-    // 노티피케이션센터를 저장하기위한 속성
+    
     var token: NSObjectProtocol?
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        /// call keyboard notification method
         keyboardWillShow()
         keyboardWillHide()
-        
-        /// configure textField's view
         [emailTextField, passwordTextField, repeatPasswordTextField, nameTextField, nickNameTextField, nextButton, changeProfilePicButton].forEach({
             $0?.layer.cornerRadius = 10
             $0?.clipsToBounds = true
             
         })
+        if let  verifiedEmail = verifiedEmail {
+            emailTextField.text = verifiedEmail
+        }
         
-        /// load Previous data in emailTextField.text
-            if let safeEmail = keyChain.get(Keys.userEmailKey) {
-                emailTextField.text = safeEmail
-            }
         
-        /// received a notification from ProfilePicturesViewController and save image data
         token = NotificationCenter.default.addObserver(forName: .didTapProfilePics, object: nil, queue: .main, using: { [weak self] noti  in
             guard let strongSelf = self else { return }
             guard let profileImage = noti.userInfo?[ProfilePicturesViewController.picsKey] as? Int else { return }
@@ -112,33 +80,27 @@ class DetailRegisterViewController: UIViewController {
             
         })
         
-        /// configure profileImageView's shape like a circle
         profileImageView.layer.cornerRadius = profileImageView.frame.width / 2.0
         profileImageView.clipsToBounds = true
         profileImageView.contentMode = .scaleAspectFill
         
         
-        /// when user didTap background made a keyboard down
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(DetailRegisterViewController.backgroundTap))
         self.view.addGestureRecognizer(tapGestureRecognizer)
-        
-        
         nameTextField.delegate = self
         repeatPasswordTextField.delegate = self
         nickNameTextField.delegate = self
         emailTextField.delegate = self
         passwordTextField.delegate = self
         
-        /// dummy data
         nameTextField.text = "황신택"
         nickNameTextField.text = "TaekToy"
-        passwordTextField.text = "Qwer1234567!"
-        repeatPasswordTextField.text = "Qwer1234567!"
+        passwordTextField.text = User.password
+        repeatPasswordTextField.text = User.password
         
         
     }
     
-    /// verify using regular expression
     func isPasswordValid(_ password : String) -> Bool{
         if let range = password.range(of: Regex.password, options: [.regularExpression]), (range.lowerBound, range.upperBound) == (password.startIndex, password.endIndex) {
             return true
@@ -147,7 +109,6 @@ class DetailRegisterViewController: UIViewController {
         return false
     }
     
-    /// verify using regular expression
     func isEmailValid(_ email: String) -> Bool {
         if let range = email.range(of: Regex.email, options: [.regularExpression]), (range.lowerBound, range.upperBound) == (email.startIndex, email.endIndex) {
             return true
@@ -156,7 +117,6 @@ class DetailRegisterViewController: UIViewController {
         return false
     }
     
-    /// remove observer
     deinit {
         if let token = token {
             NotificationCenter.default.removeObserver(token)
@@ -166,17 +126,14 @@ class DetailRegisterViewController: UIViewController {
 }
 
 extension DetailRegisterViewController: UITextFieldDelegate {
-    /// To recognize when user enter a value in textfield
     func textFieldDidBeginEditing(_ textField: UITextField) {
         activeTextField = textField
     }
 
-    /// To recognize when user stop enter a value in textfield
     func textFieldDidEndEditing(_ textField: UITextField) {
         activeTextField = nil
     }
     
-    /// emailTextField can't enter any chracters
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         if textField == emailTextField {
             let char = CharacterSet(charactersIn: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
@@ -190,8 +147,6 @@ extension DetailRegisterViewController: UITextFieldDelegate {
 
 extension DetailRegisterViewController {
     
-    
-    /// Invoke only this method when keyboard in case of contact with textField
     func keyboardWillShow() {
         NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: .main) { [weak self] noti in
             guard let strongSelf = self else { return  }
@@ -217,7 +172,6 @@ extension DetailRegisterViewController {
         }
     }
     
-    /// Make lower the keyboard
     func keyboardWillHide() {
         NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: .main) { [weak self] noti in
             guard let strongSelf = self else { return }
@@ -226,8 +180,6 @@ extension DetailRegisterViewController {
     }
     
     
-    /// once didtap background forced to make lower the keyboard
-    /// - Parameter sender: UITapGestureRecognizer
     @objc func backgroundTap(_ sender: UITapGestureRecognizer) {
         self.view.endEditing(true)
     }
--- UMate/UMate/Account/VC/RegisterViewController.swift
+++ UMate/UMate/Account/VC/RegisterViewController.swift
@@ -10,7 +10,6 @@ import DropDown
 
 class RegisterViewController: UIViewController {
     
-    // MARK: Outlet
     @IBOutlet weak var enterenceYearView: UIView!
     @IBOutlet weak var universityView: UIView!
     @IBOutlet weak var nextButton: UIView!
@@ -21,12 +20,8 @@ class RegisterViewController: UIViewController {
     
     @IBOutlet weak var universityNameField: UILabel!
     
-    var saveText = ""
-    
-    // notificationCenter를 저장하기위한 속성
     var token: NSObjectProtocol?
     
-    // DropDown타입 배열을 클로저로 초기화한 속성
     let menu: DropDown = {
         let menu = DropDown()
         menu.dataSource = [
@@ -49,13 +44,12 @@ class RegisterViewController: UIViewController {
         return menu
     }()
     
-    // 취소 메소드
+ 
     @IBAction func cancel(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
     }
     
     
-    // 학번 선택하는 메소드
     @IBAction func enterenceYearButton(_ sender: UIButton) {
         menu.show()
         menu.selectionAction = { [weak self] index, item in
@@ -66,51 +60,33 @@ class RegisterViewController: UIViewController {
     }
     
     
-    @IBAction func nextButton(_ sender: Any) {
-        guard let enterenceYearText = enterenceTextField.text,
-              enterenceYearText.count == 6,
-        let universityNameText = universityNameField.text,
-        universityNameText == saveText else {
-            alert(title: "알림", message: "입학년도 혹은 학교이름을 선택하세요.")
-            
-            return
-        }
-        
-    }
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        ///두 view를 살짝 깎는 클로저
         [enterenceYearView, nextButton].forEach { $0?.layer.cornerRadius = 10
             $0?.clipsToBounds = true
         }
         
-        /// 학번 메뉴 width, height 조정
         menu.anchorView = enterenceYearView
         guard let height = menu.anchorView?.plainView.bounds.height else { return }
         menu.bottomOffset = CGPoint(x: 0, y: height)
         menu.width = 150
         
-        /// 백그라운드 탭하면 키보드 내려감
         let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
         dismissZone.addGestureRecognizer(tap)
         dismissZone2.addGestureRecognizer(tap)
-        
-        ///대학교이름을 받아오는 노티피케이션 + 유저디폴트에 저장
+
         token = NotificationCenter.default.addObserver(forName: .didTapSendUniversityName, object: nil, queue: .main, using: { [weak self] noti in
             guard let strongSelf = self else { return }
             guard let universityName = noti.userInfo?[SearchLIstUniversityViewController.universityNameTransitionKey] as? String else { return }
             
             strongSelf.universityNameField.text = universityName
-            strongSelf.saveText = universityName
             UserDefaults.standard.set(universityName, forKey: "universityNameKey")
         })
         
         
     }
     
-    // 키보드 내리는 메소드
+    
     @objc func dismissKeyboard() {
         dismissZone2.endEditing(true)
     }
