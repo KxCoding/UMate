d74b0f5410c1938dba96265636a3a299a6383346
sanghee <sanghee0610@ajou.ac.kr>
2021-10-01 05:39:51 +0900

    [Common - Comment, Add / Timetable - Comment, Refactor] 문서화 작업, 더미데이터 추가

--- UMate/UMate/Common/Dummy.swift
+++ UMate/UMate/Common/Dummy.swift
@@ -5,6 +5,7 @@
 //  Created by 안상희 on 2021/08/06.
 //
 
+import Elliotable
 import Foundation
 import UIKit
 
@@ -148,3 +149,46 @@ let placeholderImage = UIImage(named: "dummy-image-landscape")
 
 /// dummy URL
 let tempUrl = URL(string: "https://kxcoding.com")!
+
+
+
+// MARK: - TimeTable Dummy
+/// 강의 정보를 담은 Dummy Data 리스트.
+/// - Author: 안상희
+let friendCourseList: [ElliottEvent] =
+[ElliottEvent(courseId: "F1234",
+              courseName: "자료구조",
+              roomName: "팔308",
+              professor: "교수님",
+              courseDay: .monday,
+              startTime: "09:00",
+              endTime: "10:15",
+              textColor: UIColor.white,
+              backgroundColor: .purple),
+ ElliottEvent(courseId: "F1234",
+              courseName: "자료구조",
+              roomName: "팔308",
+              professor: "교수님",
+              courseDay: .wednesday,
+              startTime: "09:00",
+              endTime: "10:15",
+              textColor: UIColor.white,
+              backgroundColor: .purple),
+ ElliottEvent(courseId: "F5678",
+              courseName: "컴퓨터그래픽스",
+              roomName: "팔1025",
+              professor: "교수님",
+              courseDay: .monday,
+              startTime: "10:30",
+              endTime: "11:45",
+              textColor: UIColor.white,
+              backgroundColor: .cyan),
+ ElliottEvent(courseId: "F5678",
+              courseName: "컴퓨터그래픽스",
+              roomName: "팔1025",
+              professor: "교수님",
+              courseDay: .thursday,
+              startTime: "10:30",
+              endTime: "11:45",
+              textColor: UIColor.white,
+              backgroundColor: .cyan)]
