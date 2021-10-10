86fd2260cc64a440d6283d21d5fa2884367bf269
Hyejeong Park <mailmelater11@gmail.com>
2021-10-10 23:59:21 +0900

    [Extension - Comment, Edit] 주석 수정 및 구현 수정
    - 주석 수정
    - 구현 보충 및 메소드 access level 변경

--- UMate/UMate/Extension/UIView+Extension.swift
+++ UMate/UMate/Extension/UIView+Extension.swift
@@ -9,19 +9,13 @@ import Foundation
 import UIKit
 
 
-/// 공통 스타일 열거형과 스타일 옵션을 적용하는 메소드를 포함하는 UIView extension
-///
-/// 적용할 공통 스타일의 열거형 케이스, 스타일을 적용하는 메소드를 추가하고, configureStyle(with:) 메소드 내부에 패턴을 등록합니다.
 extension UIView {
     
     /// 뷰에 적용할 스타일 옵션
-    ///
-    /// 케이스를 추가할 때에는 케이스와 메소드의 순서를 일치시킵니다.
     /// - Author: 박혜정(mailmelater11@gmail.com)
     enum ViewStyleOptions {
         case lightShadow
         case heavyShadow
-        case circle
         case pillShape
         case bigRoundedRect
         case smallRoundedRect
@@ -33,22 +27,20 @@ extension UIView {
     
     
     
-    /// 미리 설정된 스타일 옵션을 해당 뷰에 추가합니다.
-    /// - Parameter options: 스타일 옵션
+    /// 대상 뷰에 스타일 옵션을 적용합니다.
+    ///
+    /// 배열에 저장된 순서대로 적용됩니다.
+    /// - Parameter options: 스타일 옵션 배열
     /// - Author: 박혜정(mailmelater11@gmail.com)
     func configureStyle(with options: [ViewStyleOptions]) {
         
-        // 배열로 전달된 옵션이 루프를 돌며 적용됩니다.
-        for op in options {
-            switch op {
+        for option in options {
+            switch option {
             case .lightShadow:
                 self.applyLightShadow()
                 
             case .heavyShadow:
                 self.applyHeavyShadow()
-                
-            case .circle:
-                self.applyCircleShape()
                 
             case .pillShape:
                 applyPillShape()
@@ -76,38 +68,30 @@ extension UIView {
     }
     
     
-    /// 뷰에 원 모양을 적용합니다.
-    /// - Author: 박혜정(mailmelater11@gmail.com)
-    private func applyCircleShape() {
-        
-    }
-    
-    
     /// 뷰에 알약 모양을 적용합니다.
     /// - Author: 박혜정(mailmelater11@gmail.com)
-    private func applyPillShape() {
+    func applyPillShape() {
         self.layer.cornerRadius = self.frame.height / 2
-        
     }
     
     
     /// 상대적으로 큰 직사각형 뷰에 둥근 모서리를 적용합니다.
     /// - Author: 박혜정(mailmelater11@gmail.com)
-    private func applyBigRoundedRect() {
+    func applyBigRoundedRect() {
         self.layer.cornerRadius = self.frame.height / 6
     }
     
     
     /// 상대적으로 작은 직사각형 뷰에 둥근 모서리를 적용합니다.
     /// - Author: 박혜정(mailmelater11@gmail.com)
-    private func applySmallRoundedRect() {
+    func applySmallRoundedRect() {
         self.layer.cornerRadius = self.frame.height / 4
     }
     
     
     /// 상대적으로 얇은 테두리를 추가합니다.
     /// - Author: 박혜정(mailmelater11@gmail.com)
-    private func applyLightBorder() {
+    func applyLightBorder() {
         self.layer.borderColor = UIColor.systemGray4.cgColor
         self.layer.borderWidth = 1
     }
@@ -116,7 +100,7 @@ extension UIView {
     /// 상대적으로 중간 두께의 컬러 테두리를 추가합니다.
     /// - Parameter borderColor: 테두리의 색상
     /// - Author: 박혜정(mailmelater11@gmail.com)
-    private func applyColorBorder(with borderColor: UIColor) {
+    func applyColorBorder(with borderColor: UIColor) {
         self.layer.borderColor = borderColor.cgColor
         self.layer.borderWidth = 2
     }
@@ -124,7 +108,7 @@ extension UIView {
     
     /// 옅은 그림자를 추가합니다.
     /// - Author: 박혜정(mailmelater11@gmail.com)
-    private func applyLightShadow() {
+    func applyLightShadow() {
         self.layer.shadowColor = UIColor.lightGray.cgColor
         self.layer.shadowOpacity = 0.3
         self.layer.shadowRadius = 5
@@ -135,14 +119,18 @@ extension UIView {
     
     /// 진한 색의 그림자를 추가합니다.
     /// - Author: 박혜정(mailmelater11@gmail.com)
-    private func applyHeavyShadow() {
-        
+    func applyHeavyShadow() {
+        self.layer.shadowColor = UIColor.lightGray.cgColor
+        self.layer.shadowOpacity = 0.6
+        self.layer.shadowRadius = 5
+        self.layer.shadowOffset = CGSize(width: 1, height: 1)
+        self.layer.masksToBounds = false
     }
     
     
     /// 적용된 보더를 삭제합니다.
     /// - Author: 박혜정(mailmelater11@gmail.com)
-    private func removeBorder() {
+    func removeBorder() {
         self.layer.borderWidth = 0
     }
     
@@ -150,12 +138,12 @@ extension UIView {
     /// 배경색을 적용합니다.
     /// - Parameter color: 배경에 적용할 색상
     /// - Author: 박혜정(mailmelater11@gmail.com)
-    private func fillBackground(with color: UIColor) {
+    func fillBackground(with color: UIColor) {
         self.backgroundColor = color
     }
     
+    
     /// View의 모서리 테마 설정 메소드
-    /// - Author: ?
     func setViewTheme() {
         self.layer.cornerRadius = 14
         self.layer.borderColor = UIColor.systemGray.cgColor
